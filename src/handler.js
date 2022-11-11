9ab31bdb4da376db92ed01fac53ecb448f49d6fb
Firman Diatullah Guna Darma <darmaroyale@gmail.com>
2022-11-11 14:55:08 +0700

    Update handler.js
    menambahkan handler get note by id

--- src/handler.js
+++ src/handler.js
@@ -42,4 +42,26 @@ const getAllNotesHandler = () => ({
     notes,
   },
 });
-module.exports = { addNoteHandler, getAllNotesHandler };
+
+const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const note = notes.filter((n) => n.id === Number(id))[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
