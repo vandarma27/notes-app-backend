8dc1a988147ddf584922346e396e4a2ed298c130
Firman Diatullah Guna Darma <darmaroyale@gmail.com>
2022-11-11 14:49:18 +0700

    Update handler.js
    menambahkan handler untuk menampilkan catatan

--- src/handler.js
+++ src/handler.js
@@ -36,4 +36,10 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+module.exports = { addNoteHandler, getAllNotesHandler };
