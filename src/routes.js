dabf7a87bbe7b38c31d144cf2679e8180fea5521
Firman Diatullah Guna Darma <darmaroyale@gmail.com>
2022-11-11 15:24:12 +0700

    Update routes.js
    menambah route put

--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,11 @@ const routes = [
     path: '/notes/{id}',
     handler: getNoteByIdHandler,
   },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler:
+  },
 ];
 
 module.exports = routes;
